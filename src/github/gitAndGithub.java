4ea324f4810b9d736b28eb9ca175529113c98e01
samirapashayeva <samirapashayeva41@gmail.com>
2022-06-04 01:19:20 -0400

    3rd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -6,7 +6,7 @@ public class gitAndGithub {
 		
 		System.out.println("Today we learn github");
 		System.out.println("Torday is my day");
-		
+		System.out.println("Its time to go to sleep");
 		
 	}
 
