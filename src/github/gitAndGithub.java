18372e49f61a33d4e84f949b9bf170f1387f20e9
samirapashayeva <samirapashayeva41@gmail.com>
2022-06-04 00:12:52 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -3,8 +3,11 @@ package github;
 public class gitAndGithub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Today we learn github");
+		System.out.println("Torday is my day");
+		
+		
 	}
 
 }
